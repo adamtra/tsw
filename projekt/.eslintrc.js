eddb4286f4b9f4dc8cafa6e19165b089fe3cf535
Adam Trawiński <adam.trawinski@poczta.fm>
2019-05-16 17:29:09 +0200

    Zmiana eslint

--- projekt/.eslintrc.js
+++ projekt/.eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/essential',
     '@vue/standard',
-    '@vue/typescript'
+    '@vue/typescript',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ['error', 4],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'always'],
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
-  }
-}
+    parser: '@typescript-eslint/parser',
+  },
+};
