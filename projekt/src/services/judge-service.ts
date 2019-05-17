c78711d77eb7c29b768db6eedaf17041b7a72610
Adam Trawiński <adam.trawinski@poczta.fm>
2019-05-17 10:36:18 +0200

    Instalacja Axios

---
+++ projekt/src/services/judge-service.ts
@@ -0,0 +1,6 @@
+import axios from 'axios';
+export class JudgeService {
+    public static get() {
+        return axios.get('http://localhost:3000/sedziowie');
+    }
+}
---
+++ projekt/src/types/judge.ts
@@ -0,0 +1,6 @@
+interface Judge {
+    id: number;
+    sedzia: string;
+    kraj: string;
+}
+export default Judge;
